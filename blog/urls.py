991d605630b46c934c448229964d63f20cf4a183
sirGoulis14 <t.goulianos@gmail.com>
2024-02-08 15:26:15 +0200

    7_2

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [path('', views.post_list, name='post_list'),
-               path('post/<int:pk>/', views.post_detail, name='post_detail'), ]
+               path('post/<int:pk>/', views.post_detail, name='post_detail'),
+               path('aboutus',views.aboutus,name='aboutus')]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from django.db.models import Q
 
 
 # Create your views here.
 def post_list(request):
     cat = request.GET.get('cat', '')
+    txt = request.GET.get('txt', '')
     try:
         cat = int(cat)
     except:
         cat = False
 
     if cat is False:
-        posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        if txt == '':
+            posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+        else:
+            posts = Post.objects.filter((Q(text__contains=txt) | Q(
+                title__contains=txt)) & Q(published_date__lte=
+                                          timezone.now())).order_by('published_date')
     else:
-        posts = Post.objects.filter(category__post__published_date__lte=timezone.now()).filter(category=cat).order_by('published_date')
+        posts = (Post.objects.filter(published_date__lte=timezone.now())
+                 .filter(category=cat).order_by('published_date'))
 
     return render(request, 'blog/post_list.html', {'posts': posts})
 
@@ -22,3 +30,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def aboutus(request):
+    return render(request, 'blog/aboutus.html')
